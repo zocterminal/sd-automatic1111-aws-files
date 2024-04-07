85bfb0d9d65d12e95bedb14d22f08feeb1198ba3
Markus Schmidt <m.schmidt@emtec.com>
2024-04-07 13:54:15 +0200

    extra checks on NVIDIA and Python install.

--- src/install_debian_v4_step2.sh
+++ src/install_debian_v4_step2.sh
@@ -7,6 +7,21 @@
 
 echo 'Setting up NVIDIA-CUDA'
 
+
+#
+# check for correct debian kernel version
+#
+UNAME=$(uname -r)
+PAT="^5.10.*-amd64$"
+
+if [[ ! $UNAME =~ $PAT ]]; then
+	echo "Incorrect Debian kernel (should be 5.10...-amd64): $UNAME";
+	echo "Kernel should be: 5.10.x.xx-xxxxx-amd64";
+	echo "Kernel is       : $UNAME";
+	exit
+fi
+
+
 # source headers for debian kernel build (required for cuda build)
 # (this will fail on some AWS specific debian images, make sure you
 # run this on the Debian-11 AMI from Debian)
@@ -16,7 +31,7 @@ sudo apt -y install linux-headers-`uname -r`
 wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda_12.0.0_525.60.13_linux.run
 
 # install the driver 
-echo 'Building and installing driver ...'
+echo 'Building and installing driver ...  (this may take a few minutes)'
 echo 'Note: While cuda-install runs, you can continue with step 3 in another tab'
 echo ''
 sudo bash cuda_12.0.0_525.60.13_linux.run --driver --silent  # install drivers only
@@ -25,6 +40,7 @@ sudo bash cuda_12.0.0_525.60.13_linux.run --driver --silent  # install drivers o
 nvidia-smi
 
 # done
+echo ''
 echo 'If all is good, delete the driver package:'
 echo '   rm cuda_12*'
 echo ''
--- src/install_debian_v4_step3.sh
+++ src/install_debian_v4_step3.sh
@@ -4,6 +4,19 @@
 
 # https://techviewleo.com/how-to-install-python-on-amazon-linux-2/
 
+#
+# check for local bin in the path
+#
+PAT="^\/ome\/$USER\/bin:"
+
+if [[ ! $PATH =~ $PAT ]]; then
+    echo "/home/$USER/bin missing from path...";
+    echo "Path should be: /home/$USER/bin:/usr.....";
+    echo "Path is       : $PATH";
+    exit
+fi
+
+
 # this packages are required to build a python that is suitable for SD
 sudo apt install -y libffi-dev libssl-dev liblzma-dev libbz2-dev libncurses5-dev libsqlite3-dev libreadline-dev libtk8.6 libgdm-dev libdb4o-cil-dev libpcap-dev
 
